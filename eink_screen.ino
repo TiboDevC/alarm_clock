fb3f5e8c314ce27ee055789e3f78165bbb1e8c56
TiboDevC <tibo.dev.iot@gmail.com>
2022-04-19 16:35:52 +0200

    Initial commit

---
+++ eink_screen.ino
@@ -0,0 +1,160 @@
+#include "DEV_Config.h"
+#include "EPD.h"
+#include "GUI_Paint.h"
+#include "imagedata.h"
+#include <stdlib.h>
+
+//Create a new image cache
+uint8_t BlackImage[((EPD_3IN7_WIDTH % 8 == 0) ? (EPD_3IN7_WIDTH * EPD_3IN7_HEIGHT) / 8 : (EPD_3IN7_WIDTH / 8 + 1)) * EPD_3IN7_HEIGHT];
+
+void setup() {
+    Serial.begin(115200);
+    delay(5000);
+    Serial.println("EPD_3IN7_test Demo\r\n");
+    DEV_Module_Init();
+
+    Serial.println("e-Paper Init and Clear...\r\n");
+    EPD_3IN7_4Gray_Init();
+    EPD_3IN7_4Gray_Clear();
+    DEV_Delay_ms(500);
+
+    // UWORD Imagesize = ((EPD_3IN7_WIDTH % 8 == 0) ? (EPD_3IN7_WIDTH / 8 ) : (EPD_3IN7_WIDTH / 8 + 1)) * EPD_3IN7_HEIGHT;
+    Serial.print("Allocate ");
+    Serial.println(sizeof(BlackImage));
+    // BlackImage = (UBYTE *)malloc(Imagesize);
+    // if (BlackImage == NULL) {
+    //     Serial.println("Failed to apply for black memory...\r\n");
+    //     while (1);
+    // }
+
+    Serial.println("Paint_NewImage\r\n");
+    Paint_NewImage(BlackImage, EPD_3IN7_WIDTH, EPD_3IN7_HEIGHT, 0, BLACK);
+    // Paint_SetScale(4);
+    // Paint_Clear(WHITE);
+
+#if 0   //show image for array    
+    Serial.println("show image for array\r\n");
+    EPD_3IN7_4Gray_Display(gImage_3in7);
+    DEV_Delay_ms(2000);
+#endif
+
+#if 0 // Drawing on the image, partial display
+    //1.Select Image
+    Serial.println("SelectImage:BlackImage\r\n");
+    Paint_SelectImage(BlackImage);
+    Paint_SetScale(4);
+    Paint_Clear(WHITE);
+    // DEV_Delay_ms(5000);
+
+
+    // 2.Drawing on the image
+    Serial.println("Drawing:BlackImage");
+    Paint_DrawPoint(10, 80, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
+    Paint_DrawPoint(10, 90, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
+    Paint_DrawPoint(10, 100, BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
+    Serial.println("Paint_DrawLine");
+    Paint_DrawLine(20, 70, 70, 120, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Paint_DrawLine(70, 70, 20, 120, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+    Serial.println("Paint_DrawRectangle");
+    Paint_DrawRectangle(20, 70, 70, 120, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+    Paint_DrawRectangle(80, 70, 130, 120, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+    Serial.println("Paint_DrawCircle");
+    Paint_DrawCircle(45, 95, 20, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+    Paint_DrawCircle(105, 95, 20, WHITE, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+    Serial.println("Paint_DrawLine");
+    Paint_DrawLine(85, 95, 125, 95, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+    Paint_DrawLine(105, 75, 105, 115, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+    Serial.println("Paint_DrawString_EN");
+    Paint_DrawString_EN(10, 0, "waveshare", &Font16, BLACK, WHITE);
+    Paint_DrawString_EN(10, 20, "hello world", &Font12, WHITE, BLACK);
+    Serial.println("Paint_DrawNum");
+    Paint_DrawNum(10, 33, 123456789, &Font12, BLACK, WHITE);
+    Paint_DrawNum(10, 50, 987654321, &Font16, WHITE, BLACK);
+    Serial.println("Paint_DrawString_EN");
+    Paint_DrawString_EN(10, 150, "GRAY1 with black background", &Font24, BLACK, GRAY1);
+    Paint_DrawString_EN(10, 175, "GRAY2 with white background", &Font24, WHITE, GRAY2);
+    Paint_DrawString_EN(10, 200, "GRAY3 with white background", &Font24, WHITE, GRAY3);
+    Paint_DrawString_EN(10, 225, "GRAY4 with white background", &Font24, WHITE, GRAY4);
+    Serial.println("EPD_Display");
+    EPD_3IN7_4Gray_Display(BlackImage);
+    DEV_Delay_ms(4000);
+#endif
+
+#if 1 // partial update, just 1 Gray mode
+    EPD_3IN7_1Gray_Init();       //init 1 Gray mode
+    EPD_3IN7_1Gray_Clear();
+    Paint_SelectImage(BlackImage);
+    Paint_SetScale(2);
+    Paint_Clear(BLACK);
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            Paint_SetPixel(i, j, WHITE);
+        }
+    }
+    Paint_SetPixel(20, 20, WHITE);
+    Paint_SetPixel(20, 21, WHITE);
+    Paint_SetPixel(21, 20, WHITE);
+    Paint_SetPixel(21, 21, WHITE);
+    EPD_3IN7_1Gray_Display_Part(BlackImage, 0, 0, EPD_3IN7_HEIGHT, EPD_3IN7_WIDTH);
+
+    Paint_Clear(BLACK);
+    DEV_Delay_ms(5000);
+    Serial.println("show time, partial update, just 1 Gary mode\r\n");
+    PAINT_TIME sPaint_time;
+    sPaint_time.Hour = 12;
+    sPaint_time.Min = 34;
+    sPaint_time.Sec = 56;
+    UBYTE num = 5;
+    for (;;) {
+        sPaint_time.Sec = sPaint_time.Sec + 1;
+        if (sPaint_time.Sec == 60) {
+            sPaint_time.Min = sPaint_time.Min + 1;
+            sPaint_time.Sec = 0;
+            if (sPaint_time.Min == 60) {
+                sPaint_time.Hour =  sPaint_time.Hour + 1;
+                sPaint_time.Min = 0;
+                if (sPaint_time.Hour == 24) {
+                    sPaint_time.Hour = 0;
+                    sPaint_time.Min = 0;
+                    sPaint_time.Sec = 0;
+                }
+            }
+        }
+        Serial.println("Paint_ClearWindows");
+        Paint_ClearWindows(0, 0, EPD_3IN7_HEIGHT, EPD_3IN7_WIDTH, WHITE);
+        Serial.println("Paint_DrawTime");
+        Paint_DrawTime(0, 0, &sPaint_time, &Font20, BLACK, WHITE);
+
+        num = num - 1;
+        if (num == 0) {
+            break;
+        }
+
+        Serial.println("Part refresh...\r\n");
+        // EPD_3IN7_1Gray_Display(BlackImage);
+        EPD_3IN7_1Gray_Display_Part(BlackImage, 0, 50, EPD_3IN7_HEIGHT, EPD_3IN7_WIDTH);
+        DEV_Delay_ms(500);
+    }
+
+#endif
+    // EPD_3IN7_4Gray_Init();
+    Serial.println("Clear...\r\n");
+    // EPD_3IN7_4Gray_Clear();
+
+    // Sleep & close 5V
+    Serial.println("Goto Sleep...\r\n");
+    // EPD_3IN7_Sleep();
+
+    // free(BlackImage);
+    // BlackImage = NULL;
+
+    Serial.println("close 5V, Module enters 0 power consumption ...\r\n");
+}
+
+/* The main loop -------------------------------------------------------------*/
+void loop()
+{
+    //
+}
