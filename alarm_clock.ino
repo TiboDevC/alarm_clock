bb6a5c18a00371d3d31f23503758d1cc13446c8e
TiboDevC <tibo.dev.iot@gmail.com>
2022-04-30 21:19:57 +0200

    Init RTC

--- alarm_clock.ino
+++ alarm_clock.ino
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <RTCZero.h>
 
 #include "DEV_Config.h"
 #include "EPD.h"
@@ -6,9 +7,38 @@
 #include "imagedata.h"
 #include "screen.h"
 
+void init_rtc()
+{
+    RTCZero rtc;
+
+    const uint8_t hours{12};
+    const uint8_t minutes{45};
+    const uint8_t seconds{10};
+
+    const uint8_t day{30};
+    const uint8_t month{04};
+    const uint8_t year{22};
+
+    rtc.begin();
+
+    // Set the time
+    rtc.setHours(hours);
+    rtc.setMinutes(minutes);
+    rtc.setSeconds(seconds);
+
+
+    // Set the date
+    rtc.setDay(day);
+    rtc.setMonth(month);
+    rtc.setYear(year);
+}
+
 void setup() {
   Serial.begin(115200);
   delay(5000);
+
+  init_rtc();
+
   Serial.println("EPD_3IN7_test Demo\r\n");
   DEV_Module_Init();
 
