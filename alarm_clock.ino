1ee69a2bc1d18bdaf97d568dd63c1465f2df9fef
TiboDevC <tibo.dev.iot@gmail.com>
2022-05-01 11:49:39 +0200

    Add interrupt on RTC to update date

--- alarm_clock.ino
+++ alarm_clock.ino
@@ -7,16 +7,30 @@
 #include "imagedata.h"
 #include "screen.h"
 
+static void alarmMatch()
+{
+    RTCZero rtc;
+
+    init_screen();
+    screen_update_clock();
+
+    EPD_3IN7_Sleep();
+
+    rtc.setAlarmSeconds(0);
+    rtc.attachInterrupt(alarmMatch);
+    rtc.enableAlarm(rtc.MATCH_SS);
+}
+
 void init_rtc()
 {
     RTCZero rtc;
 
-    const uint8_t hours{12};
-    const uint8_t minutes{45};
-    const uint8_t seconds{10};
+    const uint8_t hours{23};
+    const uint8_t minutes{56};
+    const uint8_t seconds{00};
 
-    const uint8_t day{30};
-    const uint8_t month{04};
+    const uint8_t day{1};
+    const uint8_t month{05};
     const uint8_t year{22};
 
     rtc.begin();
@@ -31,6 +45,10 @@ void init_rtc()
     rtc.setDay(day);
     rtc.setMonth(month);
     rtc.setYear(year);
+
+    rtc.setAlarmSeconds(0);
+    rtc.attachInterrupt(alarmMatch);
+    rtc.enableAlarm(rtc.MATCH_SS);
 }
 
 void setup() {
