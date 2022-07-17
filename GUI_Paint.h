3a61ef5f74b471a3e2dd0f60fd63a47f7eccd7f3
TiboDevC <tibo.dev.iot@gmail.com>
2022-07-17 14:38:43 +0200

    Indent code

--- GUI_Paint.h
+++ GUI_Paint.h
@@ -78,8 +78,8 @@
 #ifndef __GUI_PAINT_H
 #define __GUI_PAINT_H
 
-#include "Debug.h"
 #include "DEV_Config.h"
+#include "Debug.h"
 #include "fonts.h"
 #include "paint.hpp"
 
@@ -87,17 +87,17 @@
  * Image attributes
 **/
 typedef struct {
-  UBYTE* Image;
-  UWORD Width;
-  UWORD Height;
-  UWORD WidthMemory;
-  UWORD HeightMemory;
-  UWORD Color;
-  UWORD Rotate;
-  UWORD Mirror;
-  UWORD WidthByte;
-  UWORD HeightByte;
-  UWORD Scale;
+    UBYTE *Image;
+    UWORD Width;
+    UWORD Height;
+    UWORD WidthMemory;
+    UWORD HeightMemory;
+    UWORD Color;
+    UWORD Rotate;
+    UWORD Mirror;
+    UWORD WidthByte;
+    UWORD HeightByte;
+    UWORD Scale;
 } PAINT;
 extern PAINT Paint;
 
@@ -113,10 +113,10 @@ extern PAINT Paint;
  * Display Flip
 **/
 typedef enum {
-  MIRROR_NONE = 0x00,
-  MIRROR_HORIZONTAL = 0x01,
-  MIRROR_VERTICAL = 0x02,
-  MIRROR_ORIGIN = 0x03,
+    MIRROR_NONE       = 0x00,
+    MIRROR_HORIZONTAL = 0x01,
+    MIRROR_VERTICAL   = 0x02,
+    MIRROR_ORIGIN     = 0x03,
 } MIRROR_IMAGE;
 #define MIRROR_IMAGE_DFT MIRROR_NONE
 
@@ -132,67 +132,67 @@ typedef enum {
 #define FONT_BACKGROUND WHITE
 
 // 4 Gray level
-#define GRAY1 0x03  // Blackest
+#define GRAY1 0x03// Blackest
 #define GRAY2 0x02
-#define GRAY3 0x01  // gray
-#define GRAY4 0x00  // white
+#define GRAY3 0x01// gray
+#define GRAY4 0x00// white
 /**
  * The size of the point
 **/
 typedef enum {
-  DOT_PIXEL_1X1 = 1,  // 1 x 1
-  DOT_PIXEL_2X2,      // 2 X 2
-  DOT_PIXEL_3X3,      // 3 X 3
-  DOT_PIXEL_4X4,      // 4 X 4
-  DOT_PIXEL_5X5,      // 5 X 5
-  DOT_PIXEL_6X6,      // 6 X 6
-  DOT_PIXEL_7X7,      // 7 X 7
-  DOT_PIXEL_8X8,      // 8 X 8
+    DOT_PIXEL_1X1 = 1,// 1 x 1
+    DOT_PIXEL_2X2,    // 2 X 2
+    DOT_PIXEL_3X3,    // 3 X 3
+    DOT_PIXEL_4X4,    // 4 X 4
+    DOT_PIXEL_5X5,    // 5 X 5
+    DOT_PIXEL_6X6,    // 6 X 6
+    DOT_PIXEL_7X7,    // 7 X 7
+    DOT_PIXEL_8X8,    // 8 X 8
 } DOT_PIXEL;
-#define DOT_PIXEL_DFT DOT_PIXEL_1X1  // Default dot pilex
+#define DOT_PIXEL_DFT DOT_PIXEL_1X1// Default dot pilex
 
 /**
  * Point size fill style
 **/
 typedef enum {
-  DOT_FILL_AROUND = 1,  // dot pixel 1 x 1
-  DOT_FILL_RIGHTUP,     // dot pixel 2 X 2
+    DOT_FILL_AROUND = 1,// dot pixel 1 x 1
+    DOT_FILL_RIGHTUP,   // dot pixel 2 X 2
 } DOT_STYLE;
-#define DOT_STYLE_DFT DOT_FILL_AROUND  // Default dot pilex
+#define DOT_STYLE_DFT DOT_FILL_AROUND// Default dot pilex
 
 /**
  * Line style, solid or dashed
 **/
 typedef enum {
-  LINE_STYLE_SOLID = 0,
-  LINE_STYLE_DOTTED,
+    LINE_STYLE_SOLID = 0,
+    LINE_STYLE_DOTTED,
 } LINE_STYLE;
 
 /**
  * Whether the graphic is filled
 **/
 typedef enum {
-  DRAW_FILL_EMPTY = 0,
-  DRAW_FILL_FULL,
+    DRAW_FILL_EMPTY = 0,
+    DRAW_FILL_FULL,
 } DRAW_FILL;
 
 /**
  * Custom structure of a time attribute
 **/
 typedef struct {
-  UWORD Year;  // 0000
-  UBYTE Month;  // 1 - 12
-  UBYTE Day;  // 1 - 30
-  UBYTE Hour;  // 0 - 23
-  UBYTE Min;  // 0 - 59
-  UBYTE Sec;  // 0 - 59
+    UWORD Year; // 0000
+    UBYTE Month;// 1 - 12
+    UBYTE Day;  // 1 - 30
+    UBYTE Hour; // 0 - 23
+    UBYTE Min;  // 0 - 59
+    UBYTE Sec;  // 0 - 59
 } PAINT_TIME;
 extern PAINT_TIME sPaint_time;
 
 // init and Clear
-void Paint_NewImage(UBYTE* image, UWORD Width, UWORD Height, UWORD Rotate,
+void Paint_NewImage(UBYTE *image, UWORD Width, UWORD Height, UWORD Rotate,
                     UWORD Color);
-void Paint_SelectImage(UBYTE* image);
+void Paint_SelectImage(UBYTE *image);
 void Paint_SetRotate(UWORD Rotate);
 void Paint_SetMirroring(UBYTE mirror);
 void Paint_SetPixel(UWORD Xpoint, UWORD Ypoint, UWORD Color);
@@ -215,22 +215,22 @@ void Paint_DrawCircle(UWORD X_Center, UWORD Y_Center, UWORD Radius, UWORD Color,
 
 // Display string
 void Paint_DrawChar(UWORD Xstart, UWORD Ystart, const char Acsii_Char,
-                    sFONT* Font, UWORD Color_Foreground,
+                    sFONT *Font, UWORD Color_Foreground,
                     UWORD Color_Background);
-void Paint_DrawString_EN(UWORD Xstart, UWORD Ystart, const char* pString,
-                         sFONT* Font, UWORD Color_Foreground,
+void Paint_DrawString_EN(UWORD Xstart, UWORD Ystart, const char *pString,
+                         sFONT *Font, UWORD Color_Foreground,
                          UWORD Color_Background);
-void Paint_DrawString_CN(UWORD Xstart, UWORD Ystart, const char* pString,
-                         cFONT* font, UWORD Color_Foreground,
+void Paint_DrawString_CN(UWORD Xstart, UWORD Ystart, const char *pString,
+                         cFONT *font, UWORD Color_Foreground,
                          UWORD Color_Background);
-void Paint_DrawNum(UWORD Xpoint, UWORD Ypoint, int32_t Nummber, sFONT* Font,
+void Paint_DrawNum(UWORD Xpoint, UWORD Ypoint, int32_t Nummber, sFONT *Font,
                    UWORD Color_Foreground, UWORD Color_Background);
-void Paint_DrawTime(UWORD Xstart, UWORD Ystart, PAINT_TIME* pTime, sFONT* Font,
+void Paint_DrawTime(UWORD Xstart, UWORD Ystart, PAINT_TIME *pTime, sFONT *Font,
                     UWORD Color_Foreground, UWORD Color_Background);
 
 // pic
-void Paint_DrawBitMap(const unsigned char* image_buffer);
-void Paint_DrawImage(const unsigned char* image_buffer, UWORD xStart,
+void Paint_DrawBitMap(const unsigned char *image_buffer);
+void Paint_DrawImage(const unsigned char *image_buffer, UWORD xStart,
                      UWORD yStart, UWORD W_Image, UWORD H_Image);
 
 #endif
