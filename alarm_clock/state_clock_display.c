d346a3f1605d926290a35d65ff2b71e2b8893661
TiboDevC <tibo.dev.iot@gmail.com>
2022-11-27 20:12:45 +0100

    First button handler in state_clock_display

--- alarm_clock/state_clock_display.c
+++ alarm_clock/state_clock_display.c
@@ -2,12 +2,30 @@
 
 #include "button.h"
 #include "debug.h"
-#include "screen.h"
 #include "rtc_tool.h"
+#include "screen.h"
 
 #define SCREEN_REFRESH_FREQ_MIN 2 /* Refresh screen every x minutes */
 
+#define STATE_CLOCK_DISPLAY_INFO(x...) debug("[state_clock_display] " x)
+
 static uint64_t _last_epoch_refresh_time;
+
+static void _handle_button(uint32_t evt)
+{
+	struct button_evt_t evt_button;
+	evt_button.button_id  = (evt >> BUTTON_ID_SHIFT) & BUTTON_ID_MSK;
+	evt_button.action     = (evt >> BUTTON_ACTION_SHIFT) & BUTTON_ACTION_MSK;
+	evt_button.push_count = (evt >> BUTTON_COUNT_SHIFT) & BUTTON_COUNT_MSK;
+
+	STATE_CLOCK_DISPLAY_INFO("Receive button: ");
+	debug_num(evt_button.button_id);
+	debug(", action: ");
+	debug_num(evt_button.action);
+	debug(", push_count: ");
+	debug_num(evt_button.push_count);
+	debug("\n");
+}
 
 enum fsm_handler_rc state_clock_display(struct fsm *fsm, struct fsm_event const *event)
 {
@@ -21,7 +39,7 @@ enum fsm_handler_rc state_clock_display(struct fsm *fsm, struct fsm_event const 
 	case FSM_EVENT_RTC_WAKE_UP:
 		if ((rtc_get_minutes() % SCREEN_REFRESH_FREQ_MIN == 0 && rtc_get_seconds() < 20) ||
 		    ((_last_epoch_refresh_time - rtc_get_epoch()) > SCREEN_REFRESH_FREQ_MIN * 60)) {
-			_last_epoch_refresh_time =  rtc_get_epoch();
+			_last_epoch_refresh_time = rtc_get_epoch();
 			debug("Update screen\n");
 			ui_update();
 		} else {
@@ -29,7 +47,7 @@ enum fsm_handler_rc state_clock_display(struct fsm *fsm, struct fsm_event const 
 		}
 		return FSM_HANDLED();
 	case FSM_EVENT_BUTTON:
-		debug("Button to be handled\n");
+		_handle_button(event->type);
 		return FSM_HANDLED();
 	default:
 		break;
