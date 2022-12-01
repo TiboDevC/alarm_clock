a7cf363e7038c722783fe877b90d0e28bfda1117
TiboDevC <tibo.dev.iot@gmail.com>
2022-12-01 13:53:25 +0100

    First step to power on speaker

--- alarm_clock/state_alarm_ongoing.c
+++ alarm_clock/state_alarm_ongoing.c
@@ -1,9 +1,12 @@
 #include "alarm_clock_fsm.h"
 
+#include "Arduino.h"
 #include "debug.h"
 #include "music.h"
 
 #define STATE_CLOCK_DISPLAY_INFO(x...) debug("[state_alarm_ongoing] " x)
+
+#define PIN_SWITCH_SPEAKER 21
 
 enum fsm_handler_rc state_alarm_ongoing(struct fsm *fsm, struct fsm_event const *event)
 {
@@ -12,6 +15,8 @@ enum fsm_handler_rc state_alarm_ongoing(struct fsm *fsm, struct fsm_event const 
 	switch (event_alarm_clock) {
 	case FSM_EVENT_ENTRY:
 		STATE_CLOCK_DISPLAY_INFO("Ring alarm!\n");
+		pinMode(PIN_SWITCH_SPEAKER, OUTPUT);
+		digitalWrite(PIN_SWITCH_SPEAKER, HIGH);
 		if (music_init() == 0) {
 			music_play();
 		} else {
@@ -19,6 +24,7 @@ enum fsm_handler_rc state_alarm_ongoing(struct fsm *fsm, struct fsm_event const 
 		}
 		return FSM_HANDLED();
 	case FSM_EVENT_EXIT:
+		digitalWrite(PIN_SWITCH_SPEAKER, LOW);
 		return FSM_HANDLED();
 	case FSM_EVENT_BUTTON:
 		/* If a button is pushed, stop the alarm */
